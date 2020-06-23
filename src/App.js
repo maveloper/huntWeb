ded3d812c87846593dd2399bc05407f3f09de113
MAY <mavcode@outlook.com>
2020-06-23 17:40:54 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello, world</h1>
+//     </div>
+//   );
+// }
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello, world</h1>
+      </div>
+    );
+  }
+}
+
+
+export default App;
