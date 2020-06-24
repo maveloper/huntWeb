37e592cde5e9c549f2ef438c050aefa58d0dbb94
MAY <mavcode@outlook.com>
2020-06-24 15:34:56 -0300

    Activate get the docs(products) to api with render method ans stats

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
+import api from './services/api'
+import Header from './components/Header';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello, world</h1>
-//     </div>
-//   );
-// }
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello, world</h1>
-      </div>
-    );
-  }
-}
+import './styles.css';
+
+import Main from './pages/main';
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+);
+
+
 
 
 export default App;
