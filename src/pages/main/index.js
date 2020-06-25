f1480a77d5271dccab5f1c5ba9c2fb6968139a9d
MAY <mavcode@outlook.com>
2020-06-25 19:16:41 -0300

    button's prev and next OK

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -9,6 +9,7 @@ export default class Main extends Component {
     state = {
         products: [],
         productInfo: {},
+        page: 1,
     }
 
 
@@ -17,42 +18,53 @@ export default class Main extends Component {
     }
 
 
-    loadProducts = async () => {
-        const response = await api.get(`/products`);
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
         const { docs, ...productInfo } = response.data;
 
 
-        this.setState({ products: response.data.docs });
+        this.setState({ products: docs, productInfo, page });
         // console.log('here', response)
         // console.log(response.data.docs);
     }
 
-    nextPage = () =>
-        prevPage = () =>
+    nextPage = () => {
+        const { page, productInfo } = this.state;
+        if (page === productInfo.pages) return;
+
+        const pageNumber = page + 1;
+        this.loadProducts(pageNumber);
+
+    }
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+        this.loadProducts(pageNumber);
+
+    }
 
 
-            render() {
-    const { products } = this.state;
-    return (
-        <div className="product-list">
-            {products.map(product => (
-                <article key={product._id}>
-                    <strong>{product.title}</strong>
-                    <p>{product.description}</p>
-                    <a href="">Acessar</a>
-                </article>
-            ))}
-            <div className="actions">
-                <button onClick={this.prevPage}>Anterior</button>
-                <button onClick={this.nextPage}>Próxima</button>
+    render() {
+        const { products, page, productInfo } = this.state;
 
+        return (
+            <div className="product-list">
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        <a href="">Acessar</a>
+                    </article>
+                ))}
+                <div className="actions">
+                    <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                    <button disables={page === productInfo.pages} onClick={this.nextPage}>Próxima</button>
+
+                </div>
             </div>
-        </div>
-    )
-
-
-
-    // <h1>Contagem de produtos: {this.state.products.length}</h1>
+        );
+    }
 }
-
-}
+    // <h1>Contagem de produtos: {this.state.products.length}</h1>}
